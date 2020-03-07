026791f3e8dccb633ab7d0ef381ed99de8b8c936
fakeProgrammer0 <greenjiachen@gmail.com>
2020-03-07 20:17:01 +0800

    update w2v.py

--- api/w2v.py
+++ api/w2v.py
@@ -7,6 +7,8 @@ import sys
 import socket
 import time
 import logging
+import gensim.matutils as matutils
+import numpy as np
 
 logger = logging.getLogger(__name__)
 logger.setLevel(logging.DEBUG)
@@ -21,11 +23,11 @@ app = Flask(__name__)
 app.config['JSON_AS_ASCII'] = False
 
 model = None
-port = 8888
+port = 28888
 localIp = "localhost"
 
 
-def isNoneWords(word):
+def is_none_words(word):
     if word is None or len(word) == 0 or word not in model.vocab:
         return True
     else:
@@ -50,7 +52,7 @@ def welcome():
 @app.route("/vec", methods=['GET'])
 def vec_route():
     word = request.args.get("word")
-    if isNoneWords(word):
+    if is_none_words(word):
         return jsonify("word is null or not in model!")
     else:
         return jsonify({'word': word, 'vector': model.word_vec(word).tolist()})
@@ -60,7 +62,7 @@ def vec_route():
 def similarity_route():
     word1 = request.args.get("word1")
     word2 = request.args.get("word2")
-    if isNoneWords(word1) or isNoneWords(word2):
+    if is_none_words(word1) or is_none_words(word2):
         return jsonify("word is null or not in model!")
     else:
         return jsonify({
@@ -73,7 +75,7 @@ def similarity_route():
 @app.route("/top_sim", methods=['GET'])
 def top_similarity_route():
     word = request.args.get("word")
-    if isNoneWords(word):
+    if is_none_words(word):
         return jsonify("word is null or not in model!")
     else:
         return jsonify({
@@ -81,6 +83,85 @@ def top_similarity_route():
             word,
             'top_similar_words':
             model.similar_by_word(word, topn=20, restrict_vocab=None)
+        })
+
+class SimilarityMeasure:
+
+    def __init__(self, label, similarity_callback):
+        self.label = label
+        self.similarity_callback = similarity_callback
+
+
+def get_similarity_measures():
+
+    def L1(A, x):
+        return 1 / np.linalg.norm(A - x, 1, axis=1)
+
+    def L2(A, x):
+        return 1 / np.sqrt(np.linalg.norm(A - x, 1, axis=1))
+
+    measures = [
+        SimilarityMeasure('cos', np.dot),
+        SimilarityMeasure('L1', L1),
+        SimilarityMeasure('L2', L2)
+    ]
+    return measures
+
+
+def most_similar_words(word, measures, top_n=10):
+    if is_none_words(word):
+        return {}
+
+    result = {}
+    vector = model.get_vector(word)
+    word_index = model.vocab[word].index
+    model.init_sims()
+    for measure in measures:
+        dists = measure.similarity_callback(model.vectors_norm, vector)
+        best = matutils.argsort(dists, topn=top_n + 1, reverse=True)
+        top_words = [(model.index2word[sim], float(dists[sim])) for sim in best if sim != word_index]
+        result[measure.label] = top_words[:top_n]
+
+    return result
+
+
+def print_similar_words(similar_words):
+    for label, word_score_pairs in similar_words.items():
+        print(label, end=' : ')
+        for word_score in word_score_pairs:
+            print("{} : {:.4f}".format(*word_score), end=' ; ')
+        print(end='\n')
+
+
+def ouput_similar_words(word, similar_words, fname=None):
+    if fname is None:
+        fname = word + "_top_similar_words.txt"
+
+    with open(fname, 'w', encoding='utf8') as out_file:
+        out_file.write('word : {}\n'.format(word))
+        for label, word_score_pairs in similar_words.items():
+            out_file.write(label + ' : ')
+            for word_score in word_score_pairs:
+                out_file.write("{} : {:.4f} ; ".format(*word_score))
+            out_file.write('\n')
+
+
+predefine_measures = get_similarity_measures()
+
+
+@app.route("/top_words", methods=['GET'])
+def top_similarity_words_route():
+    word = request.args.get("word")
+    if is_none_words(word):
+        return jsonify("word is null or not in model!")
+    else:
+        top_words = most_similar_words(word, predefine_measures)
+        ouput_similar_words(word, top_words)
+        return jsonify({
+            'word':
+            word,
+            'top_similar_words':
+            top_words
         })
 
 
